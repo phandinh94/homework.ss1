79f6ef1e6ee07d14a2409d8a5bdc9757b5f3e7de
phandinh94 <110024878+phandinh94@users.noreply.github.com>
2022-07-28 23:26:44 +0700

    BTVN28_7

---
+++ app.js
@@ -0,0 +1,33 @@
+//Bài 1: Viết chương trình nhập điểm của một sinh viên cho các môn:
+// Vật lý, Hóa học, và Sinh học.
+// Sau đó hiển thị điểm trung bình và tổng của những điểm này.
+// let a =prompt("nhap diem mon Vat li");
+// let b =prompt("nhap diem mon Hoa hoc");
+// let c =prompt("nhap diem mon Sinh hoc");
+// let tong = Number(a) + Number(b) + Number(c);
+// let dtb = tong/3;
+// alert(`tong diem 3 mon la ${tong} va diem trung binh 3 mon la ${dtb}`);
+
+//Bài 2:  Viết chương trình nhập một giá trị là độ 0C (Celsius) và chuyển nó sang độ 0F (Fahrenheit).
+// [Hướng dẫn: C/5 = (F-32)/9]
+// let C =prompt("Nhập giá trị nhiệt độ C");
+// let F = 9/5 * C + 32;
+// alert(`Độ F sau khi chuyển đổi ra được là ${F}`);
+
+//Bài 3: Viết chương trình tính diện tích hình tròn
+// let R = prompt ("Nhập bán kính");
+// let S = (R**2) * Math.PI;
+// alert(`Diện tích hình tròn có được là ${S}`);
+// hoặc hiển thị qua màn hình console:
+// let R = prompt("Nhập bán kính");
+// let S = (R**2) * Math.PI
+// console.log(`Diện tích hình tròn tính được là ${S}`);
+
+//Bài 4: Viết chương trình chu vi hình tròn
+// let R = prompt("Nhập bán kính");
+// let Cv = 2 * R * Math.PI;
+// alert(`Chu vi hình tròn là ${Cv}`);
+// hiển thị qua console
+// let R = prompt("Nhập bán kính");
+// let Cv = 2 * R * Math.PI;
+// console.log(`Chu vi hình tròn là ${S}`);
